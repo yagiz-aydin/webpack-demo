141c411fea9910a562c925387fb27a91f4e31bf2
yagiz.aydin <yagiz.aydin@azerion.com>
2022-06-05 19:22:10 +0300

    [master] Building Stage added

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
+// Absolute Path on import stage
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+// All Css files sass less styled bundle 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Minify & Optimization with building stage
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, 'src'),
   mode: 'development',
-  output: {
-   path: path.join(__dirname, 'bundle'),
-   filename: 'bundle.js'
-  },
   devServer: {
     static: 'src',
     hot: true,
@@ -32,5 +32,14 @@ module.exports = {
       title: "Webpack Demo",
       template: 'src/index.html'
     }),
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  // How Build Folder & File must be out in file structure when runs `npm run build` commands
+  output: {
+   path: path.join(__dirname, 'that_build'),
+   filename: 'that_main_build_file.js'
+  },
 };
